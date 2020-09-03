727fa30994cbaa3321fd29601af2e307738b6259
Stefano Valsecchi <stefano.valsecchi@vidiemme.lan>
2020-09-03 15:52:49 +0200

    decima prova fill

--- Sources/SPMDemo/CounterView.swift
+++ Sources/SPMDemo/CounterView.swift
@@ -14,7 +14,7 @@ public class CounterView: UIView {
     
     private struct Constants {
         static let numberOfGlasses = 8
-        static let lineWidth: CGFloat = 37.5 // 5.0
+        static let lineWidth: CGFloat = 39 // 5.0
         static let arcWidth: CGFloat = 76
     
         static var halfOfLineWidth: CGFloat {
